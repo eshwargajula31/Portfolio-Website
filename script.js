bbacaec0c530f72bee64ad5d379dff42bc5c383f
ESHWAR GAJULA <gajularajini9@gmail.com>
2024-10-25 11:20:30 +0530

    Automatic commit message

---
+++ script.js
@@ -0,0 +1,60 @@
+// Typing and Deleting effect
+document.addEventListener("DOMContentLoaded", () => {
+    const typedText = ["Web Developer", "Web Designer", "Machine Learning Enthusiast","IoT Developer","Problem Solver"];
+    let typingIndex = 0;
+    let currentIndex = 0;
+    let isDeleting = false;
+    const typingSpeed = 150; 
+    const deletingSpeed = 100; 
+    const delayBetweenWords = 2000; 
+
+    const typingElement = document.querySelector(".typing");
+
+    function type() {
+        const currentText = typedText[typingIndex];
+        
+        if (isDeleting) {
+            typingElement.textContent = currentText.slice(0, currentIndex - 1);
+            currentIndex--;
+
+            if (currentIndex === 0) {
+                isDeleting = false;
+                typingIndex = (typingIndex + 1) % typedText.length;
+                setTimeout(type, typingSpeed);
+            } else {
+                setTimeout(type, deletingSpeed);
+            }
+        } else {
+            typingElement.textContent = currentText.slice(0, currentIndex + 1);
+            currentIndex++;
+
+            if (currentIndex === currentText.length) {
+                isDeleting = true;
+                setTimeout(type, delayBetweenWords);
+            } else {
+                setTimeout(type, typingSpeed);
+            }
+        }
+    }
+
+    type();
+
+    // Hamburger Menu Toggle
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    
+
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+       
+    });
+
+    // Close menu when a link is clicked (for better UX)
+    const navItems = document.querySelectorAll(".nav-links a");
+    navItems.forEach(item => {
+        item.addEventListener("click", () => {
+            navLinks.classList.remove("active");
+            hamburger.classList.remove("toggle");
+        });
+    });
+});
